10df3311cb43e536f096cb059e4049909a098144
HeyAero <aeroyts@icloud.com>
2021-04-13 11:37:21 +0100

    update Homepage and Music components to pass down props to child components

--- src/components/Music/index.js
+++ src/components/Music/index.js
@@ -1,13 +1,7 @@
 import React, { useState } from 'react';
 import { Button, Like } from '../';
 
-const Music = () => {
-  const [music, setMusic] = useState([
-    { song: "Danny Nedelko", album: "Joy as an Act of Resistance", img: "https://upload.wikimedia.org/wikipedia/en/thumb/b/bd/Idles_-_Danny_Nedelko.jpg/220px-Idles_-_Danny_Nedelko.jpg", link: "https://www.idlesband.com/uk/"},
-    { song: "MR.MOTIVATOR", album: "Ultra Mono", img: "https://f4.bcbits.com/img/a1320226088_10.jpg", link: "https://www.idlesband.com/uk/"},
-    { song: "GROUNDS", album: "Ultra Mono", img: "https://f4.bcbits.com/img/a3789051601_10.jpg", link: "https://www.idlesband.com/uk/"}
-  ])
-
+const Music = ({music}) => {
   const renderMusic = () => {
     return music.map(m =>
       <div className="card col-sm">
--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Intro, Welcome, Music } from '../../components';
 
 const Homepage = () => {
+
+  const [music, setMusic] = useState([
+    { song: "Danny Nedelko", album: "Joy as an Act of Resistance", img: "https://upload.wikimedia.org/wikipedia/en/thumb/b/bd/Idles_-_Danny_Nedelko.jpg/220px-Idles_-_Danny_Nedelko.jpg", link: "https://www.idlesband.com/uk/"},
+    { song: "MR.MOTIVATOR", album: "Ultra Mono", img: "https://f4.bcbits.com/img/a1320226088_10.jpg", link: "https://www.idlesband.com/uk/"},
+    { song: "GROUNDS", album: "Ultra Mono", img: "https://f4.bcbits.com/img/a3789051601_10.jpg", link: "https://www.idlesband.com/uk/"}
+  ])
+
   return (
     <main className="container">
       <Intro />
       <Welcome />
-      <Music />
+      <Music music={music} />
     </main>
   )
 }
