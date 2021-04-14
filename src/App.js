d44443aa6610160a3be0ca3ff6d5bc2965c7fd7e
HeyAero <aeroyts@icloud.com>
2021-04-14 11:33:24 +0100

    update App js to include switch + route components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import { Homepage } from './pages';
+import { Header, Footer } from './layout';
 
 function App() {
   return (
     <>
-      <Homepage />
+      <Header/>
+      <Switch>
+        <Route exact path="/">
+          <Homepage />
+        </Route>
+      </Switch>
+      <Footer/>
     </>
   );
-}
+};
 
 export default App;
