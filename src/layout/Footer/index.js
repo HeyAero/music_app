0f4e2151564154f501f0571485da53a4861f5a11
HeyAero <aeroyts@icloud.com>
2021-04-14 11:30:13 +0100

    add footer layout component and update exports for all layouts

---
+++ src/layout/Footer/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Footer = () => <span id="footer">$copy; IDLES 2021</span>
+
+export default Footer;
--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -10,3 +10,5 @@ const Header = () => {
     </nav>
   );
 }
+
+export default Header;
