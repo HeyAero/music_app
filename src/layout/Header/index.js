1fffa6a38129a129118c23421ebb57da28e68988
HeyAero <aeroyts@icloud.com>
2021-04-14 11:28:53 +0100

    add Header component and layout file structure

---
+++ src/layout/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <nav>
+      <NavLink exact to="/" activeClassName="current">Home</NavLink>
+      <NavLink to="/about" activeClassName="current">About</NavLink>
+      <NavLink to="/tour" activeClassName="current">Tour Dates</NavLink>
+    </nav>
+  );
+}
